b21cc9d51452a9c18905bf52572dd0209add6cfc
NAVAN <contactnavaneeth@gmail.com>
2020-12-04 15:00:26 +1100

    Logout Redirect patch + redundant web project....

---
+++ SitecorePatch/SitecoreLogout/LogoutRedirect.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Pipelines.Logout;
+
+namespace SitecoreLogout
+{
+    public class LogoutRedirectProcessor
+    {
+        public void Process(LogoutArgs args)
+        {
+            Tracer.Info("Intercepting call to Sitecore logout. Redirecting to Single Logout instead");
+            Sitecore.Text.UrlString urlString = new Sitecore.Text.UrlString("https://www.google.com");
+            args.RedirectUrl = urlString;
+
+            //var uri = "https://www.google.com";
+            //var psi = new System.Diagnostics.ProcessStartInfo();
+            //psi.UseShellExecute = true;
+            //psi.FileName = uri;
+            //System.Diagnostics.Process.Start(psi);
+
+        }
+    }    
+}
